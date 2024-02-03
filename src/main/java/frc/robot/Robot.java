3f60f13b91340944ea07672990dfb7779ba4e5ff
Andrew Rickard <43037484+arickard-sf@users.noreply.github.com>
2024-02-03 12:57:05 -0500

    initial revision robot metadata logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -47,6 +47,9 @@ public class Robot extends LoggedRobot {
     Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+
+    Logger.recordMetadata("version","v1.0");
+
     m_robotContainer = new RobotContainer();
   }
 
