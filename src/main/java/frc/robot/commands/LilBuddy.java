c2312cab7edc1243da330ff8d34bd1018b31ca54
Eshaan Bhinder <Eshaan@Eshaans-MacBook-Air.local>
2024-02-27 15:32:38 -0500

    Added speed adjustment for little buddy command Eshaan

--- src/main/java/frc/robot/commands/LilBuddy.java
+++ src/main/java/frc/robot/commands/LilBuddy.java
@@ -3,16 +3,24 @@ package frc.robot.commands;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 
-public class LilBuddyit  extends Command{
+public class LilBuddy  extends Command{
     public TalonFX roller;
     
     @Override
     public void initialize() {
         roller = new TalonFX(14);
         roller.set(.1);
+        SmartDashboard.putNumber("Set Speed", 0);
+
+    }
+    @Override
+    public void execute() {
+        double speed=SmartDashboard.getNumber("Set Speed", 0);
+        roller.set(speed);
     }
     @Override
     public void end(boolean interrupted) {
