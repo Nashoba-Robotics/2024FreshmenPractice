a53ac9490782354062910e48517979f7fca27596
Tristan Miller <2022tmiller@mynrsd.com>
2024-03-14 16:10:45 -0400

    saved project good this time

--- src/main/java/frc/robot/commands/LidarTestCommand.java
+++ src/main/java/frc/robot/commands/LidarTestCommand.java
@@ -1,28 +1,53 @@
 package frc.robot.commands;
 
+import java.util.Optional;
+
 import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.drive.DriveSubsystem;
 
 public class LidarTestCommand extends Command {
     
+    public DriveSubsystem drive;
+
     public Counter counter;
     public DigitalInput source;
 
     public Timer timer;
 
     public double lastDist;
+    public double lastRobotX;
     public double currentDist;
+    public double currentRobotX;
     public double lastTime;
     public double currentTime;
     public LinearFilter filter;
 
-    public double velocity;
+    public double lidarVelocity;
+    public double robotVelocity;
+    public double oponentVelocity;
+
+    public double robotToMidline;
+    public double oponentToMidline;
+
+    public boolean doWin;
+
+    public double robotX;
+    public double oponentX;
+    public Alliance alliance;
+    public Optional<Alliance> ally;
 
     public LidarTestCommand(){
+        drive = RobotContainer.drive;
+        ally = DriverStation.getAlliance();
+
         source = new DigitalInput(0);
         counter = new Counter(source);
         counter.setMaxPeriod(1);
@@ -58,10 +83,27 @@ public class LidarTestCommand extends Command {
         lastTime = currentTime;
         currentTime = timer.get();
 
-        velocity = Math.abs((currentDist - lastDist) / (currentTime - lastTime));
+        lidarVelocity = Math.abs((currentDist - lastDist) / (currentTime - lastTime));
 
         SmartDashboard.putNumber("Object Dist", getDist());
-        SmartDashboard.putNumber("Object Velocity", velocity);
+        SmartDashboard.putNumber("Object lidarVelocity", lidarVelocity);
+
+        lastRobotX = robotX;
+        robotX = drive.getPose().getX();
+
+        if(ally.get() == Alliance.Blue) oponentX = robotX + getDist();
+        else oponentX = robotX - getDist();
+
+        robotVelocity = Math.abs((robotX - lastRobotX) / (currentTime - lastTime));
+        oponentVelocity = lidarVelocity - robotVelocity;
+
+        robotToMidline = Math.abs(8.29 - robotX);
+        oponentToMidline = Math.abs(8.29 - oponentX);
+
+        if((oponentToMidline / oponentVelocity) > (robotToMidline / robotVelocity)) doWin = false;
+        else doWin = true;
+
+        SmartDashboard.putBoolean("Win Race To Note?", doWin);
 
     }
 
